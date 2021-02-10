88fb1349193df02eef44faced6e82fac47f38abb
Deivid Willyan <deivid.fabiano.trd@c6bank.com>
2021-02-10 15:23:26 -0300

    add animation to show or hide likes/dislikes

--- TInderApp/Controller/CombineVC.swift
+++ TInderApp/Controller/CombineVC.swift
@@ -52,7 +52,7 @@ extension CombineVC {
 extension CombineVC {
     
     @objc func handlerCard(_ gesture: UIPanGestureRecognizer) {
-        if let card = gesture.view {
+        if let card = gesture.view as? CombineCardView {
             let point = gesture.translation(in: view)
             
             card.center = CGPoint(
@@ -62,12 +62,23 @@ extension CombineVC {
             
             let rotationAngle = point.x / view.bounds.width * 0.4
             
+            if point.x > 0 {
+                card.likeImageView.alpha = rotationAngle * 5
+                card.deslikeImageView.alpha = 0
+            } else {
+                card.likeImageView.alpha = 0
+                card.deslikeImageView.alpha = (rotationAngle * 5) * -1
+            }
+            
             card.transform = CGAffineTransform(rotationAngle: rotationAngle)
             
             if gesture.state == .ended {
                 UIView.animate(withDuration: 0.2) {
                     card.center = self.view.center
                     card.transform = .identity
+                    
+                    card.likeImageView.alpha = 0
+                    card.deslikeImageView.alpha = 0
                 }
             }
             
