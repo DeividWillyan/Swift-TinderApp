dcee23a51aec974f75dbb9f8736451476f307ff8
Deivid Willyan <deivid.fabiano.trd@c6bank.com>
2021-02-10 13:48:58 -0300

    Initial Commit

---
+++ TInderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TInderApp
+//
+//  Created by Deivid Willyan Rodrigues Fabiano on 10/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
